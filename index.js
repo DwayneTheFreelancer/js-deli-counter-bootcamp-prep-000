b9302ec3ea3f8ab0bd3a4235ad8c5e75e49862a7
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-31 18:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(num, name) {
   if(num.length >= 0) {
-    
+    katzDeliLine.push(name);
   }
   return `Welcome, ${name}. You are number ${num} in line.`;
 }
