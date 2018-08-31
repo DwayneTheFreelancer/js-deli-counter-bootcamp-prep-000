a449ca311a16fa5d708bd7206b40a3e6bf01ff11
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-31 18:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
-
     return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return "There is nobody waiting to be served!";
-
-
   }
 }
